a72c38e166de99877371d61731347dc102152c2e
Rami <habache.rami@gmail.com>
2022-02-22 11:07:32 +0100

    database

--- database/initialization.sql
+++ database/initialization.sql
@@ -9,11 +9,7 @@ CREATE TABLE `users` (
     email VARCHAR(50) NOT NULL,
     phone CHAR(10) NOT NULL,
     website VARCHAR(50) NOT NULL,
-<<<<<<< HEAD
     password CHAR(60) NOT NULL
-=======
-    password VARCHAR(50) NOT NULL DEFAULT "11111"
->>>>>>> 281b28e8a3f318012318c21d1b7db56e788e453e
 ) ENGINE = InnoDB;
 
 CREATE TABLE `posts` (
